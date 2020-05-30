78a008af03ab142180af4d37c78fe1ccf8a9f267
Joshua Stoll <jds21171@uga.edu>
2020-05-30 16:28:46 -0400

    created index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Momandme1!@",
+  database: "employeesDB"
+});
