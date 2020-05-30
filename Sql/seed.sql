de6809ff645c393c926becd67e02b9701aaf1d22
Joshua Stoll <jds21171@uga.edu>
2020-05-30 16:16:23 -0400

    created sql database files

---
+++ Sql/seed.sql
@@ -0,0 +1,10 @@
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 125000, 3), ("Legal Team Lead", 250000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3), ("Mike", "Chan", 2, 1), ("Ashley", "Rodriguez", 3, null), ("Kevin", "Tupik", 4, 3), ("Malia", "Brown", 5, null), ("Sarah", "Lourd", 2, null), ("Tom", "Allen", 4, 7), ("Christian", "Eckenrode", 1, 2)
