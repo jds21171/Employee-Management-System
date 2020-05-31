dd948bb231c0f4ac23045b5181274f5eb22a6231
Joshua Stoll <jds21171@uga.edu>
2020-05-31 01:59:01 -0400

    fixed a typo

--- index.js
+++ index.js
@@ -168,16 +168,16 @@ function addRole() {
 function updateEmployeeRole() {
     inquirer.prompt([
         {
-            message: "which employee would you like to update? (use first name only for now)",
+            message: "which employee would you like to update? (enter employee id number)",
             type: "input",
-            name: "name"
+            name: "id"
         }, {
             message: "enter the new role ID:",
             type: "number",
             name: "role_id"
         }
     ]).then(function (response) {
-        connection.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [response.role_id, response.name], function (err, data) {
+        connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [response.role_id, response.id], function (err, data) {
             if (err) throw err;
             console.table(data);
         })
